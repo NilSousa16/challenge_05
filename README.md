0d5493d79ad539f9a4825b938abbb4d62820753a
NilSousa16 <nil.sousa16@gmail.com>
2020-06-10 18:03:35 -0300

    First project

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# challenge_05
-Challenge 05
+# Challenge 05 - First Project Node.js
+
+Application with initial concepts about React Native	
+
+### Description
+
+In this challenge, it was necessary to create an application to practice new features learned for NodeJS.
+
+The created application allows you to add and list information.
+
+ ### Configuration for Development
+
+Use the package manager to install the project's dependencies.
+```sh
+yarn
+```
+
+### Tests
+
+To run the tests use the commands below:
+```sh
+yarn test
+```
+
---
+++ src/repositories/TransactionsRepository.ts
@@ -0,0 +1,65 @@
+import Transaction from '../models/Transaction';
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class TransactionsRepository {
+  private transactions: Transaction[];
+
+  constructor() {
+    this.transactions = [];
+  }
+
+  public all(): Transaction[] {
+    const transactionsReturn = this.transactions;
+
+    return transactionsReturn;
+  }
+
+  public getBalance(): Balance {
+    /**
+     * Array.prototype.reduce()
+     */
+    const { income, outcome } = this.transactions.reduce(
+      // acumulador e valores
+      (accumullator: Balance, transaction: Transaction) => {
+        if (transaction.type === 'income') {
+          accumullator.income += transaction.value;
+        } else {
+          accumullator.outcome += transaction.value;
+        }
+
+        return accumullator;
+      },
+      // formato e inicialização dos acumuladores
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
+  }
+
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
+  }
+}
+
+export default TransactionsRepository;
